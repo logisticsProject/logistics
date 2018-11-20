a304290d3e1b1467214ce05824163caaec43794f
weemlyn <1067718901@qq.com>
2018-11-20 10:43:27 +0800

    回执单更改

--- src/main/java/com/sixbang/logistics/mapper/ReceiptorderMapper.java
+++ src/main/java/com/sixbang/logistics/mapper/ReceiptorderMapper.java
@@ -10,13 +10,21 @@ public interface ReceiptorderMapper {
 
     int deleteByExample(ReceiptorderExample example);
 
+    int deleteByPrimaryKey(Integer rcId);
+
     int insert(Receiptorder record);
 
     int insertSelective(Receiptorder record);
 
     List<Receiptorder> selectByExample(ReceiptorderExample example);
 
+    Receiptorder selectByPrimaryKey(Integer rcId);
+
     int updateByExampleSelective(@Param("record") Receiptorder record, @Param("example") ReceiptorderExample example);
 
     int updateByExample(@Param("record") Receiptorder record, @Param("example") ReceiptorderExample example);
+
+    int updateByPrimaryKeySelective(Receiptorder record);
+
+    int updateByPrimaryKey(Receiptorder record);
 }
