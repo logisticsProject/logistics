f92570b21cc91ca28c0c2f9187fc445228b7b4ef
smilewangjialong <15173292439@163.com>
2018-11-23 15:16:05 +0800

    新增 职员、司机实体类

---
+++ src/main/java/com/sixbang/logistics/domain/CarEmp.java
@@ -0,0 +1,118 @@
+package com.sixbang.logistics.domain;
+
+/**
+ * 司机管理
+ */
+public class CarEmp {
+
+    //员工类型的编号 = 角色id
+    private Integer id;
+    //员工类型
+    private String descr;
+    //员工编号
+    private String numbering;
+    //员工姓名
+    private String name;
+    //工作场地
+    private String workplace;
+    //角色id
+    private Integer roleId;
+    //汽车ID
+    private Integer carId;
+    //车牌号
+    private String licensePlate;
+    //路线id
+    private String routeId;
+    //初始线路
+    private String startplace;
+    //终止地
+    private String endplace;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getNumbering() {
+        return numbering;
+    }
+
+    public void setNumbering(String numbering) {
+        this.numbering = numbering;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId;
+    }
+
+    public String getStartplace() {
+        return startplace;
+    }
+
+    public void setStartplace(String startplace) {
+        this.startplace = startplace;
+    }
+
+    public String getEndplace() {
+        return endplace;
+    }
+
+    public void setEndplace(String endplace) {
+        this.endplace = endplace;
+    }
+}
---
+++ src/main/java/com/sixbang/logistics/domain/Emp.java
@@ -0,0 +1,75 @@
+package com.sixbang.logistics.domain;
+
+public class Emp {
+
+    //员工类型的编号 = 角色id
+    private Integer id;
+    //员工类型
+    private String descr;
+    //员工编号
+    private String numbering;
+    //员工姓名
+    private String name;
+    //工作场地
+    private String workplace;
+    //角色id
+    private Integer roleId;
+    //汽车ID
+    private Integer carId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getNumbering() {
+        return numbering;
+    }
+
+    public void setNumbering(String numbering) {
+        this.numbering = numbering;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWorkplace() {
+        return workplace;
+    }
+
+    public void setWorkplace(String workplace) {
+        this.workplace = workplace;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+}
